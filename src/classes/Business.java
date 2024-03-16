f930a984fd2322a60e2ab1ce7cb5d38d98f085dd
sonephyo <sonephyo7777777@gmail.com>
2024-03-16 13:29:40 -0400

    importing previous project

---
+++ src/classes/Business.java
@@ -0,0 +1,48 @@
+package classes;
+
+public class Business {
+
+    private String business_id;
+    private String name;
+    private String address;
+    private String state;
+    private String postal_code;
+    private double stars;
+
+    public double getStars() {
+        return stars;
+    }
+
+    public String getBusiness_id() {
+        return business_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    @Override
+    public String toString() {
+        return "Business{" +
+                "business_id='" + business_id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", state='" + state + '\'' +
+                ", postal_code='" + postal_code + '\'' +
+                ", stars=" + stars +
+                '}';
+    }
+}
+
